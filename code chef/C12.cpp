46ee330fb5e8fffc61755fad8353da3d674506de
Shivangiyadav112 <80055390+Shivangiyadav112@users.noreply.github.com>
2022-07-22 13:25:45 +0530

    APPLORNG

---
+++ code chef/C12.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X, A,B;
+	cin>>X>>A>>B;
+	if(X>=(A+B))
+	    cout<<"YES"<<endl;
+	   else
+	    cout<<"NO"<<endl;
+	return 0;
+}
